5211403f4b62ae8f4dec2f45226c28abc8de69e0
Raquel Baeta <raquelcbaeta.sa@gmail.com>
2024-07-12 12:48:11 +0200

    Update cleaning_wgi.R

--- cleaning_wgi.R
+++ cleaning_wgi.R
@@ -1,59 +1,39 @@
 # Start 
 
+# title: "Cleaning economic indicators datasets"
+# author: "Raquel Baeta"
+# date: "2024-07-12"
+
 # Install necessary libraries
 install.packages(c("readxl", "tidyr", "tidyverse", "dplyr"))
 
-# Load Libraries
-library(c(readxl, tidyr, tidyverse, dplyr))
+# Load libraries
+library(c(readxl, tidyr, tidyverse, dplyr)
 
-# Set wtd
+# Set working directory
 setwd("~/Desktop/working-sessions/cleaning_data")
 
-# Read the data 
-wgi <- read_excel("wgi_world_bank.xlsx", 
-                  sheet = "Data")
+# Read the data from Excel file
+wgi <- read_excel("wgi_world_bank.xlsx", sheet = "Data")
 
-# Assuming df is your dataframe
-wgi_filtered <- wgi[grepl("Estimate", wgi$series_name), ]
-sapply(wgi_filtered, class)
+# Filter rows where 'series_name' contains 'Estimate'
+wgi_filtered <- wgi %>% filter(str_detect(series_name, "Estimate"))
 
-# Assuming wgi_filtered is your dataframe
-wgi_numeric <- wgi_filtered %>%
-  mutate_at(vars("1996":"2019"), as.numeric)
-sapply(wgi_numeric, class)
+# Convert columns 1996 to 2019 to numeric
+wgi_numeric <- wgi_filtered %>% mutate(across(`1996`:`2019`, as.numeric))
 
-# Filter the columns
+# Select relevant columns
 wgi_transformed <- wgi_numeric %>%
-  select(series_code, country, code, "1996":"2019")
+  select(series_code, country, code, `1996`:`2019`) %>%
+  pivot_longer(cols = `1996`:`2019`, names_to = "year", values_to = "value") %>%
+  filter(series_code %in% c("CC.EST", "GE.EST", "RQ.EST", "RL.EST", "VA.EST", "PV.EST")) %>%
+  rename(estimate = value) %>%
+  pivot_wider(names_from = series_code, values_from = estimate)
 
-# Reshape the data frame
-wgi_transformed <- wgi_transformed %>%
-  pivot_longer(cols = "1996":"2019", 
-               names_to = "year", 
-               values_to = "value")
+# Save the transformed data as a .csv file
+write.csv(wgi_transformed, file = "~/Desktop/working-sessions/cleaning_data/wgi_data.csv", row.names = FALSE)
 
-# Filter the rows
-wgi_transformed <- wgi_transformed %>%
-  filter(series_code %in% c("CC.EST", "GE.EST", "RQ.EST", "RL.EST", "VA.EST", "PV.EST"))
-
-# Rename the value column
-wgi_transformed <- wgi_transformed %>%
-  rename(estimate = value)
-wgi_transformed
-
-# Pivot wider
-wgi_transformed_wide <- wgi_transformed %>%
-  pivot_wider(names_from = series_code, 
-              values_from = estimate)
-wgi_transformed_wide
-
-# Save as a .csv
-write.csv(
-  wgi_transformed_wide, 
-  file = "~/Desktop/working-sessions/cleaning_data/wgi_data.csv", 
-  row.names = FALSE)
-
-# Save as a .rds
-saveRDS(wgi_transformed_wide, "~/Desktop/working-sessions/wgi_data.csv.rds")
+# Save the transformed data as a .rds file
+saveRDS(wgi_transformed, file = "~/Desktop/working-sessions/cleaning_data/wgi_data.rds")
 
 # End
